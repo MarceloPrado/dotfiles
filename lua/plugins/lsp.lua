fb78eeb870a94afda05fce4de4bafcdfbcbd4b8a
Marcelo T Prado <mprado@brex.com>
2024-11-19 15:36:31 -0300

    Minor fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,47 @@
 return {
   "neovim/nvim-lspconfig",
-  config = function()
-    require("lspconfig").relay_lsp.setup({})
-    require("lspconfig").vtsls.setup({
-      autoUseWorkspaceTsdk = true,
-      settings = {
-        typescript = {
-          tsserver = {
-            maxTsServerMemory = 16384,
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      vtsls = {
+        autoUseWorkspaceTsdk = true,
+        settings = {
+          typescript = {
+            tsserver = {
+              maxTsServerMemory = 16384,
+            },
           },
         },
       },
-    })
-    require("lspconfig").biome.setup({
-      cmd = { "biome", "lsp-proxy" },
-      filetypes = {
-        "css",
-        "graphql",
-        "javascript",
-        "javascriptreact",
-        "json",
-        "jsonc",
-        "typescript",
-        "typescript.tsx",
-        "typescriptreact",
+      biome = {
+        cmd = { "biome", "lsp-proxy" },
+        filetypes = {
+          "css",
+          "graphql",
+          "javascript",
+          "javascriptreact",
+          "json",
+          "jsonc",
+          "typescript",
+          "typescript.tsx",
+          "typescriptreact",
+        },
+        root_dir = require("lspconfig.util").root_pattern("biome.json", "biome.jsonc"),
+        single_file_support = false,
       },
-      root_dir = require("lspconfig.util").root_pattern("biome.json", "biome.jsonc"),
-      single_file_support = false,
-    })
-  end,
+      -- Add Relay LSP configuration
+      relay_lsp = {
+        filetypes = {
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        root_dir = require("lspconfig.util").root_pattern("relay.config.*", "package.json"),
+        cmd = { "relay-compiler", "lsp" },
+        -- You can customize these options as needed
+        auto_start_compiler = false,
+        path_to_config = nil,
+      },
+    },
+  },
 }

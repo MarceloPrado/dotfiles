7335c97b6584fbb687b5fce092b9ff08290d3d49
Marcelo Terreiro Prado <mprado@brex.com>
2026-03-23 13:30:23 -0300

    feat(nvim): improve git workflow

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,4 +1,53 @@
+local function open_neogit(args)
+  return function()
+    require("neogit").open(args)
+  end
+end
+
 return {
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      keymaps = {
+        view = {
+          { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+        },
+        file_panel = {
+          { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+        },
+        file_history_panel = {
+          { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "Close diffview" } },
+        },
+      },
+    },
+  },
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", open_neogit({ kind = "tab" }), desc = "Open Neogit" },
+      { "<leader>gc", open_neogit({ "commit", kind = "split" }), desc = "Neogit commit" },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      kind = "split",
+      commit_editor = {
+        kind = "split",
+        staged_diff_split_kind = "split",
+      },
+      popup = {
+        kind = "split",
+      },
+      integrations = {
+        diffview = true,
+        fzf_lua = true,
+      },
+    },
+  },
   {
     "lewis6991/gitsigns.nvim",
     opts = {
@@ -8,6 +57,12 @@ return {
         delete = { text = "_" },
         topdelete = { text = "‾" },
         changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged_enable = true,
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 300,
       },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
@@ -18,7 +73,20 @@ return {
 
         map("n", "]h", gs.next_hunk, "Next hunk")
         map("n", "[h", gs.prev_hunk, "Previous hunk")
+        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
         map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+        map("n", "<leader>hi", gs.preview_hunk_inline, "Preview hunk inline")
+        map("n", "<leader>hb", function()
+          gs.blame_line({ full = true })
+        end, "Blame line")
+        map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle line blame")
+        map("x", "<leader>hs", function()
+          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, "Stage hunk")
+        map("x", "<leader>hr", function()
+          gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, "Reset hunk")
       end,
     },
   },
